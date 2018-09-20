d1fc524e704cfecd06eee113dd9f0850bd122103
Jeroen Meijer <jeroenfkmeijer@gmail.com>
2018-09-20 23:05:05 +0900

    Version 1.0.0
    Added:
    - Add Dart 2 compatibility.
    - Add `bool random` argument to `getUsers` function to get random users from the data source.
    - Add custom Gender enumerator.
    - Add `getUser` function to retrieve a single user.
    - Add argument validation to `getUsers` function.
    - Add documentation to `getUsers` function.
    
    Changed:
    - Add extra info to README.
    - Change `RandomUser`'s `userName` field to `username`.
    - Change RandomUser.gender `String` to a custom `Gender` enumerator value.
    - Fixed up various spelling errors.
    
    Removed:
    - Removed `dart_dev` tooling dependencies for Dart 2 compatibility.

--- lib/random_users.dart
+++ lib/random_users.dart
@@ -1,4 +1,5 @@
 library random_users;
 
 export 'src/random_users_base.dart';
-export 'package:random_users/src/random_user.dart';
+export 'package:random_users/src/model/random_user.dart';
+export 'package:random_users/src/model/gender.dart';
